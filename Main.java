93cb6ff34a6ab76a11162b6861aa440b70d02f75
dennismasinde <chengwadennismasinde@gmail.com>
2023-05-27 15:00:22 +0300

    add word count and reverse word recursively

--- Main.java
+++ Main.java
@@ -40,7 +40,12 @@ public class Main {
         System.out.println(deleteOccurence("dennis", 'n'));
         System.out.println(convertStringToInteger("34567") + 1);
         System.out.println(convertIntegerToString(3456));
-
+        String paragraph = """
+                Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented,[14] and specifically designed to have as few implementation dependencies as possible. It is intended to let application developers "write once, run anywhere" (WORA),[15] meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.[16] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of computer architecture. As of 2016, Java is one of the most popular programming languages in use,[17][18][19][20] particularly for client-server web applications, with a reported 9 million developers.[21] Java was originally developed by James Gosling at Sun Microsystems (which has since been acquired by Oracle Corporation) and released in 1995 as a core component of Sun Microsystems' Java platform. The language derives much of its syntax from C and C++, but it has fewer low-level facilities than either of them.
+                The original and reference implementation Java compilers, virtual machines, and class libraries were originally released by Sun under proprietary licenses. As of May 2007, in compliance with the specifications of the Java Community Process, Sun relicensed most of its Java technologies under the GNU General Public License. Others have also developed alternative implementations of these Sun technologies, such as the GNU Compiler for Java (bytecode compiler), GNU Classpath (standard libraries), and IcedTea-Web (browser plugin for applets).
+                The latest version is Java 8 which is the only version currently supported for free by Oracle, although earlier versions are supported both by Oracle and other companies on a commercial basis.""";
+        wordFrequency(paragraph);
+        System.out.println(reverseStringRecursively("dennis"));
 
     }
 
@@ -83,6 +88,18 @@ public class Main {
         return newString.toString();
     }
 
+    public static String reverseStringRecursively(String word){
+        String wordReversed = "";
+
+        if (word.length() == 1){
+            return word;
+        } else {
+            wordReversed = wordReversed.concat(String.valueOf(word.charAt(word.length() - 1)));
+            wordReversed = wordReversed.concat(reverseString(word.substring(0,word.length() - 1)));
+        }
+        return wordReversed;
+    }
+
     public static String palindromeCheck(String str1) {
         str1 = str1.toLowerCase().replaceAll("\\s","");
         String str2 = reverseString(str1);
@@ -93,6 +110,20 @@ public class Main {
             }
         }
         return "Palindrome";
+    }
+
+    public static void wordFrequency(String paragraph) {
+        String[] words = paragraph.split(" ");
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        map.forEach((k,v) -> System.out.println("Key = " + k + ", Value = " + v));
     }
 
     public static String convertToUppercase(String str) {
